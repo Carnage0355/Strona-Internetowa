83691949097bd218a49e298cd6d2c7a316c826bc
unknown <patryk.sluzbowy035@gmail.com>
2025-10-24 21:00:15 +0200

    Projekt2

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wedkowanie</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<header>
+<h1>Portal dla wedkarzy</h1> 
+</header>
+
+<main>
+
+<div id="left">
+<div id="list">
+    <h3>Ryby zamieszkujące rzeki</h3>
+    <ol>
+        <li>Skrypt1</li>
+    </ol>
+</div>
+<div id="table">
+    <h3>Ryby drapieżne naszych wód</h3>
+        <table>
+            <tr>
+                <th>L.p.</th>
+                <th>Gatunek</th>
+                <th>Wystepowanie</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+</div>
+
+<div id="right">
+<img src="sum.jpg" alt="Sum">
+<a href="kwerendy.txt" target='blank'>Pobierz kwerendy</a>
+</div>
+
+<footer>
+<p>Stronę wykonał: Patryk P.</p>
+</footer>
+
+</main>
+</body>
+</html>
